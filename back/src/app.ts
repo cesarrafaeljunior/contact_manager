5a6f18770cd0679fc19652af9c17f75d4d8f989d
César Rafael <cesarrdev@gmail.com>
2023-03-22 12:32:48 -0300

    feat: Added route /client

--- back/src/app.ts
+++ back/src/app.ts
@@ -3,9 +3,14 @@ import "reflect-metadata";
 import express from "express";
 import "dotenv/config";
 import { errorHandler } from "./errors/errorhandler.errors";
+import { clientRoutes } from "./routes/client.routes";
+import { contactRoutes } from "./routes/contacts.routes";
 
 const app = express();
 app.use(express.json());
+
+app.use("/client", clientRoutes);
+app.use("/contact", contactRoutes);
 
 app.use(errorHandler);
 
