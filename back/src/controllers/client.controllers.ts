dc28fbfb8e1759ad9bb08ce6e223a33a6d9c5600
César Rafael <cesarrdev@gmail.com>
2023-03-22 16:35:53 -0300

    feat: Returning response from client data

--- back/src/controllers/client.controllers.ts
+++ back/src/controllers/client.controllers.ts
@@ -1,3 +1,9 @@
 import { Request, Response } from "express";
+import { createClientService } from "../services/client.services";
 
-export const createClientController = async (req: Request, res: Response) => {};
+export const createClientController = async (req: Request, res: Response) => {
+  const client = req.body;
+  const clientResponse = await createClientService(client);
+
+  return res.status(201).json(clientResponse);
+};
