ef081fb93c5d28f6243362a60ec28f7ffd516b4b
César Rafael <cesarrdev@gmail.com>
2023-03-22 14:46:54 -0300

    feat: Schema configuration for clients

--- back/src/schemas/client.schemas.ts
+++ back/src/schemas/client.schemas.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const clientSchema = z.object({
-  fullName: z.string().max(50),
-  username: z.string().max(50),
-  email: z.string().email().max(50),
-  password: z.string().min(8).max(10),
-  telephone: z.string().max(10).nullable(),
+  fullName: z.string({ required_error: "fullName is required" }).max(50),
+  username: z.string({ required_error: "username is required" }).max(50),
+  email: z.string({ required_error: "email is required" }).email().max(50),
+  password: z.string({ required_error: "password is required" }).min(8).max(10),
+  telephone: z.string().max(10).optional(),
 });
