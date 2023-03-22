a8a1edfd4e3c19373abf4445d75a76ee5e8319bc
César Rafael <cesarrdev@gmail.com>
2023-03-22 11:57:46 -0300

    feat: Custom error handling setup

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,8 +1,12 @@
+import "express-async-errors";
 import "reflect-metadata";
 import express from "express";
 import "dotenv/config";
+import { errorHandler } from "./errors/errorhandler.errors";
 
 const app = express();
 app.use(express.json());
+
+app.use(errorHandler);
 
 export default app;
