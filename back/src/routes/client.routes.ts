fddcb7d695a7b1a5ce232264d675da58c2f4480e
César Rafael <cesarrdev@gmail.com>
2023-03-22 11:47:54 -0300

    feat:Created folder and configure files routes

---
+++ back/src/routes/client.routes.ts
@@ -0,0 +1,8 @@
+// import { Router } from "express";
+
+// export const clientRoutes = Router();
+
+// clientRoutes.post();
+// clientRoutes.get();
+// clientRoutes.patch();
+// clientRoutes.delete();
---
+++ back/src/routes/contacts.routes.ts
@@ -0,0 +1,8 @@
+// import { Router } from "express";
+
+// export const contactRoutes = Router();
+
+// contactRoutes.post();
+// contactRoutes.get();
+// contactRoutes.patch();
+// contactRoutes.delete();
