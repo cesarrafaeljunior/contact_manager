9d4d9a761fb0ae042faa564b7a3318b56d88acf5
César Rafael <cesarrdev@gmail.com>
2023-03-22 14:46:22 -0300

    Added schema validation middleware

--- back/src/routes/client.routes.ts
+++ back/src/routes/client.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createClientController } from "../controllers/client.controllers";
+import { verifyMiddleware } from "../middlewares/verifyDataMiddleware.middlewares";
+import { clientSchema } from "../schemas/client.schemas";
 
 export const clientRoutes = Router();
 
-clientRoutes.post("", createClientController);
+clientRoutes.post("", verifyMiddleware(clientSchema), createClientController);
 // clientRoutes.get();
 // clientRoutes.patch();
 // clientRoutes.delete();
