170ecf86304a475dff7cb50de1b1958987adfba8
César Rafael <cesarrdev@gmail.com>
2023-03-23 13:41:44 -0300

    fix: Remove server owner verification from deleting clients

--- back/src/services/client/deleteClient.service.ts
+++ back/src/services/client/deleteClient.service.ts
@@ -3,14 +3,7 @@ import { AppDataSource } from "../../data-source";
 import { Client } from "../../entities/client.entity";
 import { AppError } from "../../errors/apperror.errors";
 
-export const deleteClientService = async (
-  clientId: string,
-  clientLoggedId: string
-): Promise<{}> => {
-  if (clientId != clientLoggedId) {
-    throw new AppError("Permission denied", 401);
-  }
-
+export const deleteClientService = async (clientId: string): Promise<{}> => {
   const clientRepository = AppDataSource.getRepository(Client);
 
   const client = await clientRepository.findOneBy({
