f2c081193cbbf70bad8ee30abe7212066f13ee6c
César Rafael <cesarrdev@gmail.com>
2023-03-23 02:49:12 -0300

    Creating the Client Deletion Controller

--- back/src/controllers/client.controllers.ts
+++ back/src/controllers/client.controllers.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { IClientRequest } from "../interfaces/client.interface";
-import { createClientService } from "../services/client.services";
+import { verifyUuidSchema } from "../schemas/client.schemas";
+import { createClientService } from "../services/client/createClient.service";
+import { deleteClientService } from "../services/client/deleteClient.service";
 
 export const createClientController = async (req: Request, res: Response) => {
   const client: IClientRequest = req.body;
   const clientResponse = await createClientService(client);
 
   return res.status(201).json(clientResponse);
+};
+
+export const deleteClientController = async (req: Request, res: Response) => {
+  const clientId: string = req.params.id;
+  await deleteClientService(clientId);
+
+  return res.status(204).json();
 };
