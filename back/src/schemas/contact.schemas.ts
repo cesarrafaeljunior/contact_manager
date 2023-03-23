c8571ee437ade224415da4087412b5ceab0668af
César Rafael <cesarrdev@gmail.com>
2023-03-23 16:50:24 -0300

    feat: contactSchema alterable

--- back/src/schemas/contact.schemas.ts
+++ back/src/schemas/contact.schemas.ts
@@ -3,5 +3,10 @@ import { z } from "zod";
 export const contactSchema = z.object({
   fullName: z.string({ required_error: "fullName is required" }).max(50),
   email: z.string({ required_error: "email is required" }).email().max(50),
-  telephone: z.string().max(10).optional(),
+  telephone: z.string().max(10).optional().nullable(),
+});
+
+export const returnContactSchema = contactSchema.extend({
+  id: z.string().optional(),
+  createdAt: z.date().optional(),
 });
