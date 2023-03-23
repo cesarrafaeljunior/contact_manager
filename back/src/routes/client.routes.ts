7d149d49b21fbd50523ea861ca240c44683013af
César Rafael <cesarrdev@gmail.com>
2023-03-23 13:40:11 -0300

    feat: Added schema, token, and account owner verification middlewares

--- back/src/routes/client.routes.ts
+++ back/src/routes/client.routes.ts
@@ -6,9 +6,11 @@ import {
 } from "../controllers/client.controllers";
 import { verifySchemaMiddleware } from "../middlewares/verifyDataMiddleware.middlewares";
 import { verifyUuidMiddleware } from "../middlewares/verifyIsValidUuid.middleware";
+import { verifyOwnerAccountMiddleware } from "../middlewares/verifyOwnerAccountMiddleware.middleware";
 import { verifyTokenMiddleware } from "../middlewares/verifyTokenMiddleware.middleware";
 import {
   createClientSchema,
+  updateClientSchema,
   verifyUuidSchema,
 } from "../schemas/client.schemas";
 
@@ -23,11 +25,15 @@ clientRoutes.post(
 clientRoutes.patch(
   "/:id",
   verifyUuidMiddleware(verifyUuidSchema),
+  verifySchemaMiddleware(updateClientSchema),
+  verifyTokenMiddleware,
+  verifyOwnerAccountMiddleware,
   updateClientController
 );
 clientRoutes.delete(
   "/:id",
   verifyUuidMiddleware(verifyUuidSchema),
   verifyTokenMiddleware,
+  verifyOwnerAccountMiddleware,
   deleteClientController
 );
