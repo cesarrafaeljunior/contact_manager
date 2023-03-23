663af6dfc3fd03d4c2d4311080f610ff0f931512
César Rafael <cesarrdev@gmail.com>
2023-03-23 01:03:07 -0300

    Changed clientSchema name to createClientSchema

--- back/src/routes/client.routes.ts
+++ back/src/routes/client.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { createClientController } from "../controllers/client.controllers";
 import { verifyMiddleware } from "../middlewares/verifyDataMiddleware.middlewares";
-import { clientSchema } from "../schemas/client.schemas";
+import { createClientSchema } from "../schemas/client.schemas";
 
 export const clientRoutes = Router();
 
-clientRoutes.post("", verifyMiddleware(clientSchema), createClientController);
+clientRoutes.post(
+  "",
+  verifyMiddleware(createClientSchema),
+  createClientController
+);
 // clientRoutes.get();
 // clientRoutes.patch();
 // clientRoutes.delete();
--- back/src/schemas/client.schemas.ts
+++ back/src/schemas/client.schemas.ts
@@ -1,18 +1,24 @@
 import { z } from "zod";
 
-export const clientSchema = z.object({
+const createClientSchema = z.object({
   fullName: z.string({ required_error: "fullName is required" }).max(50),
   username: z.string({ required_error: "username is required" }).max(50),
   email: z.string({ required_error: "email is required" }).email().max(50),
   password: z.string({ required_error: "password is required" }).min(8).max(10),
-  telephone: z.string().max(10).optional(),
+  telephone: z.string().max(10).nullable().optional(),
 });
 
-export const clientWhitoutPassword = z.object({
-  id: z.string().uuid().optional(),
-  fullName: z.string().optional(),
-  username: z.string().optional(),
-  email: z.string().email().optional(),
-  telephone: z.string().nullable(),
-  createdAt: z.date().optional(),
+const returnClientSchema = createClientSchema.extend({
+  id: z.string(),
+  createdAt: z.date(),
 });
+
+const returnClientSchemaWithoutPassword = returnClientSchema.omit({
+  password: true,
+});
+
+export {
+  createClientSchema,
+  returnClientSchema,
+  returnClientSchemaWithoutPassword,
+};
