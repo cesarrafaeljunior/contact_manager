9e580f9fb9c69fd0b187e2af60ceb1808008d192
César Rafael <cesarrdev@gmail.com>
2023-03-23 16:49:21 -0300

    feat: Create controller of Contact

--- back/src/controllers/contact.controllers.ts
+++ back/src/controllers/contact.controllers.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
+import { createContactService } from "../services/contact/createContact.service";
 
-export const createContactController = async (
-  req: Request,
-  res: Response
-) => {};
+export const createContactController = async (req: Request, res: Response) => {
+  const contact = req.body;
+  const clientLoggedId = req.client.id;
+
+  const contactResponse = await createContactService(contact, clientLoggedId);
+
+  return res.status(201).json(contactResponse);
+};
