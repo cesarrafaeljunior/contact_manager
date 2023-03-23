191b952bf8be769ce6954ebf87a9f59da88d2392
César Rafael <cesarrdev@gmail.com>
2023-03-23 16:49:57 -0300

    feat: Add createContactController in routes

--- back/src/routes/contacts.routes.ts
+++ back/src/routes/contacts.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createContactController } from "../controllers/contact.controllers";
 import { verifySchemaMiddleware } from "../middlewares/verifyDataMiddleware.middlewares";
+import { verifyTokenMiddleware } from "../middlewares/verifyTokenMiddleware.middleware";
 import { contactSchema } from "../schemas/contact.schemas";
 
 export const contactRoutes = Router();
@@ -8,6 +9,7 @@ export const contactRoutes = Router();
 contactRoutes.post(
   "",
   verifySchemaMiddleware(contactSchema),
+  verifyTokenMiddleware,
   createContactController
 );
 // contactRoutes.get();
