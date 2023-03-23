6f86101086b26a698993474e2950627070befeb1
César Rafael <cesarrdev@gmail.com>
2023-03-23 02:50:05 -0300

    Added ZodTypeAny schema typing

--- back/src/middlewares/verifyDataMiddleware.middlewares.ts
+++ back/src/middlewares/verifyDataMiddleware.middlewares.ts
@@ -1,8 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodTypeAny } from "zod";
-import { AppError } from "../errors/apperror.errors";
 
-export const verifyMiddleware =
+export const verifySchemaMiddleware =
   (schema: ZodTypeAny) =>
   async (req: Request, res: Response, next: NextFunction) => {
     const validatedData = await schema.parse(req.body);
