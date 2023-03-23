613408db6dea74aadd35fa0409c875107bbec651
César Rafael <cesarrdev@gmail.com>
2023-03-23 01:02:15 -0300

    Changed function from safeParse to Parse

--- back/src/middlewares/verifyDataMiddleware.middlewares.ts
+++ back/src/middlewares/verifyDataMiddleware.middlewares.ts
@@ -1,15 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodTypeAny } from "zod";
 import { AppError } from "../errors/apperror.errors";
 
 export const verifyMiddleware =
-  (schema: any) => async (req: Request, res: Response, next: NextFunction) => {
-    const validatedData = await schema.safeParse(req.body);
+  (schema: ZodTypeAny) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const validatedData = await schema.parse(req.body);
+    req.body = validatedData;
 
-    if (!validatedData.success) {
-      const formattedErrors = validatedData.error.format();
-      throw new AppError(formattedErrors, 400);
-    }
-
-    req.body = validatedData.data;
     return next();
   };
