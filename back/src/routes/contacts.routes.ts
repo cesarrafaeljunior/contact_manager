e6b79dd485d2eacad65345ad04ad907fad050c2b
César Rafael <cesarrdev@gmail.com>
2023-03-24 10:13:15 -0300

    feat: Creating patch route for contacts

--- back/src/routes/contacts.routes.ts
+++ back/src/routes/contacts.routes.ts
@@ -3,11 +3,15 @@ import {
   createContactController,
   deleteContactController,
   retrieveContactController,
+  updateContactController,
 } from "../controllers/contact.controllers";
 import { verifySchemaMiddleware } from "../middlewares/verifyDataMiddleware.middlewares";
 import { verifyUuidMiddleware } from "../middlewares/verifyIsValidUuid.middleware";
 import { verifyTokenMiddleware } from "../middlewares/verifyTokenMiddleware.middleware";
-import { verifyUuidSchema } from "../schemas/client.schemas";
+import {
+  updateClientSchema,
+  verifyUuidSchema,
+} from "../schemas/client.schemas";
 import { contactSchema } from "../schemas/contact.schemas";
 
 export const contactRoutes = Router();
@@ -19,7 +23,13 @@ contactRoutes.post(
   createContactController
 );
 contactRoutes.get("", verifyTokenMiddleware, retrieveContactController);
-// contactRoutes.patch();
+contactRoutes.patch(
+  "/:id",
+  verifyUuidMiddleware(verifyUuidSchema),
+  verifySchemaMiddleware(updateClientSchema),
+  verifyTokenMiddleware,
+  updateContactController
+);
 contactRoutes.delete(
   "/:id",
   verifyUuidMiddleware(verifyUuidSchema),
