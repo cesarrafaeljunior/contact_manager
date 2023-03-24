56b9b775eccb931c3b181a6df9d4afcb763176b7
César Rafael <cesarrdev@gmail.com>
2023-03-24 10:14:31 -0300

    feat: Schema creation to validate contact update input data

--- back/src/schemas/contact.schemas.ts
+++ back/src/schemas/contact.schemas.ts
@@ -3,10 +3,13 @@ import { z } from "zod";
 export const contactSchema = z.object({
   fullName: z.string({ required_error: "fullName is required" }).max(50),
   email: z.string({ required_error: "email is required" }).email().max(50),
-  telephone: z.string().max(10).optional().nullable(),
+  telephone: z.string().max(10),
+  client: z.any(),
 });
 
 export const returnContactSchema = contactSchema.extend({
   id: z.string().optional(),
   createdAt: z.date().optional(),
 });
+
+export const updateContactSchema = contactSchema.partial();
