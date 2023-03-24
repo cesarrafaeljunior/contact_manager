cbc2ef848f93727e8e6b1e9227355e821dd5151a
César Rafael <cesarrdev@gmail.com>
2023-03-24 10:09:46 -0300

    feat: Created interface to update contacts

--- back/src/interfaces/contact.interface.ts
+++ back/src/interfaces/contact.interface.ts
@@ -1,5 +1,8 @@
+import { DeepPartial } from "typeorm";
 import { z } from "zod";
 import { contactSchema, returnContactSchema } from "../schemas/contact.schemas";
 
 export type IContactRequest = z.infer<typeof contactSchema>;
 export type IContactRturn = z.infer<typeof returnContactSchema>;
+export type IUpdateContact = DeepPartial<IContactRequest>;
+export type IUpdateContactReturn = IContactRequest;
