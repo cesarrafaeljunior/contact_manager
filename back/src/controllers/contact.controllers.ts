d0a41cb725bffb5eafe85fd5c4103449e12eeaee
César Rafael <cesarrdev@gmail.com>
2023-03-24 00:51:54 -0300

    feat: Created retrieveContactController

--- back/src/controllers/contact.controllers.ts
+++ back/src/controllers/contact.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { createContactService } from "../services/contact/createContact.service";
 import { deleteContactService } from "../services/contact/deleteContact.service";
+import { retrieveContactService } from "../services/contact/retrieveContact.service";
 
 export const createContactController = async (req: Request, res: Response) => {
   const contact = req.body;
@@ -9,6 +10,17 @@ export const createContactController = async (req: Request, res: Response) => {
   const contactResponse = await createContactService(contact, clientLoggedId);
 
   return res.status(201).json(contactResponse);
+};
+
+export const retrieveContactController = async (
+  req: Request,
+  res: Response
+) => {
+  const clientId = req.client.id;
+
+  const contactsResponse = await retrieveContactService(clientId);
+
+  return res.status(200).json(contactsResponse);
 };
 
 export const deleteContactController = async (req: Request, res: Response) => {
