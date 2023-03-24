2b7c8ef1841fbc3f92479d50d2f32c6595ffeeda
César Rafael <cesarrdev@gmail.com>
2023-03-23 23:36:00 -0300

    feat: Create file contact controller delete

--- back/src/controllers/contact.controllers.ts
+++ back/src/controllers/contact.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createContactService } from "../services/contact/createContact.service";
+import { deleteContactService } from "../services/contact/deleteContact.service";
 
 export const createContactController = async (req: Request, res: Response) => {
   const contact = req.body;
@@ -8,4 +9,12 @@ export const createContactController = async (req: Request, res: Response) => {
   const contactResponse = await createContactService(contact, clientLoggedId);
 
   return res.status(201).json(contactResponse);
+};
+
+export const deleteContactController = async (req: Request, res: Response) => {
+  const contactId = req.params.id;
+
+  await deleteContactService(contactId);
+
+  return res.status(204).json();
 };
