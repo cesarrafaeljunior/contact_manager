4d6004b0739098530841ba47a167db905c007ed8
César Rafael <cesarrdev@gmail.com>
2023-03-26 10:03:57 -0300

    configurations of Routes

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
+import { theme } from "./style/theme";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
