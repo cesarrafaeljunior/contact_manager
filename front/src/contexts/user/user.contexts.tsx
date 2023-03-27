65b314a564ce3b9f6aef670b925678310f9c15b1
César Rafael <cesarrdev@gmail.com>
2023-03-27 02:16:13 -0300

    feat: Created function to loginUser

--- front/src/contexts/user/user.contexts.tsx
+++ front/src/contexts/user/user.contexts.tsx
@@ -1,11 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { IChildren } from "../../interfaces/react/children.type";
-import { IUserRegister } from "../../interfaces/user/user.interface";
+import {
+  IUserLogin,
+  IUserRegister,
+} from "../../interfaces/user/user.interface";
 import { request } from "../../services/axios.service";
 
 export const userContext = createContext<any>([] as any);
 
 export const UserProvider = ({ children }: IChildren) => {
+  const [token, setToken] = useState<object>({});
+  const navigate = useNavigate();
+
   const signUpUser = async (data: IUserRegister) => {
     request
       .post("client", data)
@@ -13,8 +20,25 @@ export const UserProvider = ({ children }: IChildren) => {
       .catch((error) => console.log(error));
   };
 
+  const loginUser = (data: IUserLogin) => {
+    request
+      .post("login", data)
+      .then((response) => {
+        const { token } = response.data;
+
+        request.defaults.headers.authorization = `Bearer ${token}`;
+
+        setToken(token);
+
+        localStorage.clear();
+        localStorage.setItem("contactsM: token", token);
+        navigate("/dashboard", { replace: true });
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <userContext.Provider value={{ signUpUser }}>
+    <userContext.Provider value={{ signUpUser, loginUser, token, setToken }}>
       {children}
     </userContext.Provider>
   );
