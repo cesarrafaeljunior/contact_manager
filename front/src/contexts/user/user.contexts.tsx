9ad791557c47e6b1a167eab4163f90241376b0de
César Rafael <cesarrdev@gmail.com>
2023-03-27 01:43:44 -0300

    feat: Added function signUpUser

--- front/src/contexts/user/user.contexts.tsx
+++ front/src/contexts/user/user.contexts.tsx
@@ -1,8 +1,21 @@
 import { createContext } from "react";
 import { IChildren } from "../../interfaces/react/children.type";
+import { IUserRegister } from "../../interfaces/user/user.interface";
+import { request } from "../../services/axios.service";
 
 export const userContext = createContext<any>([] as any);
 
 export const UserProvider = ({ children }: IChildren) => {
-  return <userContext.Provider value={{}}>{children}</userContext.Provider>;
+  const signUpUser = async (data: IUserRegister) => {
+    request
+      .post("client", data)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <userContext.Provider value={{ signUpUser }}>
+      {children}
+    </userContext.Provider>
+  );
 };
