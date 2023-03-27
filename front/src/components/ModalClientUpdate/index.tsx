80f26f4a6ac1411e653ef2e2463f6293b5999cc2
César Rafael <cesarrdev@gmail.com>
2023-03-27 16:56:38 -0300

    feat: Added infos for user logged

---
+++ front/src/components/ModalClientUpdate/index.tsx
@@ -0,0 +1,116 @@
+import { EmailIcon, InfoIcon, LockIcon, PhoneIcon } from "@chakra-ui/icons";
+import {
+  IconButton,
+  Icon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  FormControl,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  Text,
+} from "@chakra-ui/react";
+import { Controller, useForm } from "react-hook-form";
+import { FaUserPlus } from "react-icons/fa";
+import { useUserContext } from "../../hooks/user/useUserContext.hook";
+import { IContactRegister } from "../../interfaces/contacts/contacts.interface";
+
+export const ModalClientUpdate = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { control, handleSubmit, reset } = useForm<IContactRegister>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      telephone: "",
+    },
+  });
+
+  const { registerContact } = useUserContext();
+  return (
+    <>
+      <Text onClick={onOpen}>My profile</Text>
+      <Modal size="xs" isOpen={isOpen} onClose={onClose} motionPreset="none">
+        <ModalOverlay backdropFilter="auto" backdropBlur="2px" />
+        <ModalContent zIndex="5000">
+          <ModalHeader
+            alignSelf="center"
+            color="#5693A1"
+            fontWeight="700"
+            fontSize="1.5rem"
+          >
+            Create a contact
+          </ModalHeader>
+          <ModalBody paddingBottom="2rem">
+            <FormControl
+              onSubmit={handleSubmit((data) => {
+                registerContact(data);
+                reset({
+                  fullName: "",
+                  email: "",
+                  telephone: "",
+                });
+              })}
+              display="flex"
+              flexDirection="column"
+              gap="2rem"
+              as="form"
+            >
+              <Controller
+                name="fullName"
+                control={control}
+                render={({ field }) => (
+                  <InputGroup>
+                    <InputLeftElement children={<InfoIcon />} />
+                    <Input placeholder="full name" {...field} />
+                  </InputGroup>
+                )}
+              />
+              <Controller
+                name="email"
+                control={control}
+                render={({ field }) => (
+                  <InputGroup>
+                    <InputLeftElement children={<EmailIcon />} />
+                    <Input placeholder="email" {...field} />
+                  </InputGroup>
+                )}
+              ></Controller>
+              <Controller
+                name="telephone"
+                control={control}
+                render={({ field }) => (
+                  <InputGroup>
+                    <InputLeftElement children={<PhoneIcon />} />
+                    <Input placeholder="telephone" {...field} />
+                  </InputGroup>
+                )}
+              ></Controller>
+              <Button
+                type="submit"
+                width="80%"
+                alignSelf="center"
+                padding="1.5rem"
+                colorScheme={"blue"}
+                fontSize="1.2rem"
+                borderRadius="0.2rem"
+                onClick={() => {
+                  onClose();
+                }}
+              >
+                To create
+              </Button>
+            </FormControl>
+          </ModalBody>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
